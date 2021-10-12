b1ef8c8ea03524110700f33b85e7a4858b0cefce
3cg1 <3cg1@S30214>
2021-10-12 15:04:06 +0200

    Dodaj pliki projektów.

---
+++ ppo12102/ppo12102.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    fstream myFile;
+    myFile.open("hasla.txt");
+    string linia;
+    do
+    {
+        getline(myFile, linia);
+        cout << linia << endl;
+
+    } while (linia != "");
+    myFile.close();
+    return 0;
+}
